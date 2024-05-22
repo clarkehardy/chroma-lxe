316591d6ae0f53d2e75e7dbb1c207841e469d7eb
Sam Young <youngsam@stanford.edu>
2024-05-22 01:55:24 +0000

    clean up test script and enable event saving

--- macros/test.py
+++ macros/test.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 
     from geometry import build_detector_from_yaml
     
-    photon_pos = (0.,0.,0.)
+    photon_pos = (0.,-10.,0.)
     
     g = build_detector_from_yaml('../geometry/stl/detector.yaml', flat=True)
-    g.bvh = load_bvh(g)
+    g.bvh = load_bvh(g, read_bvh_cache=True)
 
-    sim = Simulation(g,geant4_processes=0, seed=123)
+    sim = Simulation(g,geant4_processes=0, seed=123, photon_tracking=True)
 
     # photon bomb from center
     def photon_bomb(n,wavelength,pos):
@@ -41,38 +41,22 @@ if __name__ == '__main__':
         else:
             return np.count_nonzero(np.bitwise_and(flags, test) == test)
 
-    # writer = RootWriter('out.root', g)
-    for ev in sim.simulate([photon_bomb(10000, 500, photon_pos)],keep_photons_beg=False,keep_photons_end=True,run_daq=False,max_steps=100):
+    from chroma.io.root import RootWriter
+    f = RootWriter('out.root', g)
+    for ev in sim.simulate([photon_bomb(100000, 175, photon_pos)],keep_photons_beg=True,keep_photons_end=True,run_daq=True,max_steps=100):
         # write the python event to a root file
-        # f.write_event(ev)
+        f.write_event(ev)
         detected = (ev.photons_end.flags & SURFACE_DETECT).astype(bool)
 
         photon_detection_efficiency = detected.sum()/len(detected)
         print('in event loop')
         print('# detected', detected.sum(), '# photons', len(detected))
-        print(ev.photons_end.pos)
         print("fraction of detected photons: %f"%photon_detection_efficiency)
 
         p_flags = ev.photons_end.flags
         print("\tDetect", count_test(p_flags, SURFACE_DETECT))
-        print("\tScintillation", count_test(p_flags, SCINTILLATION))
-        print("\tScintillation&SurfaceDetect", count_test(p_flags, SCINTILLATION | SURFACE_DETECT))
-        print("\tCherenkov", count_test(p_flags, CHERENKOV))
-        print("\tCherenkov No Reemit", count_test(p_flags, CHERENKOV, none_of=BULK_REEMIT))
-        print("\tCherenkov&SurfaceAbsorb", count_test(p_flags, CHERENKOV | SURFACE_ABSORB))
-        print("\tCherenkov&SurfaceDetect", count_test(p_flags, CHERENKOV | SURFACE_DETECT))
-        print("\tCherenkov&SurfaceDetect No Reemit", count_test(p_flags, CHERENKOV | SURFACE_DETECT, none_of=BULK_REEMIT))
-        print("\tCherenkov&BulkReemit&SurfaceDetect", count_test(p_flags, CHERENKOV | BULK_REEMIT | SURFACE_DETECT))
-        print("\tCherenkov&Rayleigh&SurfaceDetect", count_test(p_flags, CHERENKOV | RAYLEIGH_SCATTER | SURFACE_DETECT))
-        print(
-            "\tCherenkov&BulkReemit&Rayleigh&SurfaceDetect",
-            count_test(p_flags, CHERENKOV | BULK_REEMIT | RAYLEIGH_SCATTER | SURFACE_DETECT),
-        )
-        print("\tCherenkov&BulkAbsorb", count_test(p_flags, CHERENKOV | BULK_ABSORB))
-        print("\tCherenkov&BulkReemit", count_test(p_flags, CHERENKOV | BULK_REEMIT))
-        print("\tCherenkov&Rayleigh", count_test(p_flags, CHERENKOV | RAYLEIGH_SCATTER))
-        print("\tCherenkov&BulkReemit&Rayleigh", count_test(p_flags, CHERENKOV | BULK_REEMIT | RAYLEIGH_SCATTER))
         print("\tNoHit", count_test(p_flags, NO_HIT))
         print("\tAbort", count_test(p_flags, NAN_ABORT))
         print('\tSurfaceAbsorb', count_test(p_flags, SURFACE_ABSORB))
         print('\tBulkAbsorb', count_test(p_flags, BULK_ABSORB))
+    f.close()
