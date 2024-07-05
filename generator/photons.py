455f7859a34108bac92a620f6e5a109352332820
Sam Young <youngsam@stanford.edu>
2024-07-05 16:20:39 -0700

    document photons.py

--- generator/photons.py
+++ generator/photons.py
@@ -4,6 +4,24 @@ from chroma.event import Photons
 
 
 def create_photon_bomb(n: int, wavelength: float, pos: np.ndarray) -> Photons:
+    """Create a collection of photons at a given position with random directions.
+    
+    Parameters
+    ----------
+    n : int
+        The number of photons to create.
+    wavelength : float
+        The wavelength of the photons.
+    pos : array-like
+        The position of the photons.
+        
+    Returns
+    -------
+    photons : chroma.event.Photons
+        The collection of photons
+
+    """
+
     pos = np.tile(pos, (n, 1))
     dir = uniform_sphere(n)
     pol = np.cross(dir, uniform_sphere(n))
