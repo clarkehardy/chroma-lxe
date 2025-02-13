cf982711aeffd7e98f32078d9aa8ac5f7a75806b
clarkehardy <cahardy@stanford.edu>
2025-02-13 14:21:30 -0800

    fixed for plotting on multilens computer

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
 export PYRAT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH="$PYRAT_ROOT:${PYTHONPATH}"
 export PATH="$PYRAT_ROOT:${PATH}"
+export QT_XKB_CONFIG_ROOT="/usr/share/X11/xkb"
+export LIBGL_ALWAYS_INDIRECT=1
